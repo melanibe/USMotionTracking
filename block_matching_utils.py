2de26458156c6240aa53168c5577ead28f16ca57
melanibe <melanibe@student.ethz.ch>
2019-04-22 13:17:13 +0200

    added find template and search grid

---
+++ block_matching_utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Find pixels around a given center
+def find_search_pixel(c1, c2, width=100):
+    xax = np.linspace(c1-width/2,c1+width/2,51,dtype='int')
+    yax = np.linspace(c2-width/2,c2+width/2,51,dtype='int')
+    #xax = np.arange(int(c1)-25, int(c1)+26, step=1)
+    #yax = np.arange(int(c2)-25, int(c2)+26, step=1)
+    return np.meshgrid(xax, yax) # all the x,y in the template centered around c1, c2
+
+def find_template_pixel(c1, c2, width=100):
+    w = width//2
+    xax = np.arange(int(c1)-w, int(c1)+w+1, step=1)
+    yax = np.arange(int(c2)-w, int(c2)+w+1, step=1)
+    return np.meshgrid(xax, yax) # all the x,y in the template centered around c1, c2
