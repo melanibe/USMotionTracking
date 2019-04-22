0b628ea60286d5b932ffbc11a1dce0135f3887f0
melanibe <melanibe@student.ethz.ch>
2019-04-22 16:13:40 +0200

    across subject cross validation

---
+++ custom_KFold.py
@@ -0,0 +1,75 @@
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from block_matching_utils import find_template_pixel
+from sklearn.model_selection import KFold
+from train_local_tracking import get_all_local_features
+import sys
+
+class MyKFold:
+    def __init__(self, data_dir, n_splits=3, width_template=60, bins=20):
+        self.width_template = width_template
+        self.n_bins = bins
+        self.n_splits = n_splits
+        self.data_dir = data_dir
+        self.listdir = np.asarray([os.path.join(data_dir, dI) for dI in os.listdir(self.data_dir) if (
+            os.path.isdir(os.path.join(data_dir, dI))
+            and not dI == 'feats_matrices')])
+        self.listdir.sort()
+        print(self.listdir.shape)
+        num_dir = len(self.listdir)
+        if self.n_splits > num_dir:
+            print('Warning to many splits reducing to {} splits'.format(num_dir))
+            self.n_splits = num_dir
+            self.dir_per_fold = 1
+        else:
+            self.dir_per_fold = num_dir//self.n_splits
+        print('Number folders per fold: {}'.format(self.dir_per_fold))
+
+    def getIterator(self):
+        p = 0
+        while p < len(self.data_dir):
+            test_indices = np.arange(p, min(len(self.data_dir), p+self.dir_per_fold), dtype='int')
+            print(test_indices)
+            test_dirs = self.listdir[test_indices]
+            print(test_dirs)
+            train_dirs = np.delete(self.listdir, test_indices)
+            print(train_dirs)
+            p += self.dir_per_fold
+            X_train, x_train, y_train = get_all_local_features(train_dirs, self.width_template, self.n_bins)
+            X_test, x_test, y_test = get_all_local_features(test_dirs, self.width_template, self.n_bins)
+            yield X_train, X_test, x_train, x_test, y_train, y_test
+
+
+if __name__=='__main__':
+    data_dir = os.getenv('DATA_PATH')
+    print(data_dir)
+    kf = MyKFold(data_dir, n_splits=5)
+    iterator = kf.getIterator()
+    mse_x = []
+    mse_y = []
+    done = False
+    while not done:
+        try:
+            X_train, X_test, x_train, x_test, y_train, y_test = next(iterator)
+            print(len(X_train))
+            print(len(x_train))
+            print(len(y_train))
+            est_x = RandomForestRegressor(n_estimators=1000)
+            est_x.fit(X_train, x_train.ravel())
+            est_y = RandomForestRegressor(n_estimators=1000)
+            est_y.fit(X_train, y_train.ravel())
+            x_pred = est_x.predict(X_test)
+            y_pred = est_y.predict(X_test)
+            print(np.mean((x_pred - x_test)**2))
+            print(np.mean((y_pred - y_test)**2))
+            mse_x.append(np.mean((x_pred - x_test)**2))
+            mse_y.append(np.mean((y_pred - y_test)**2))
+        except StopIteration:
+            done = True
+    print('Mean MSE for x across fold {}, std {}'.format(np.mean(mse_x), np.std(mse_x)))
+    print('Mean MSE for y across fold {}, std {}'.format(np.mean(mse_y), np.std(mse_y)))
--- train_local_tracking.py
+++ train_local_tracking.py
@@ -9,87 +9,98 @@ from block_matching_utils import find_template_pixel
 from sklearn.model_selection import KFold
 
 
-def get_local_features(data_dir, width_template=60, bins=20):
-    listdir = [dI for dI in os.listdir(
-        data_dir) if (
-            os.path.isdir(os.path.join(data_dir, dI))
-            and not dI == 'feats_matrices')]
-    print(listdir)
-    listdir.sort()
+def get_local_features(current_dir, width_template=60, bins=20):
     feats_intensity = None
     labels_x = None
     labels_y = None
-    # x_init = None
-    # y_init = None
-    for subfolder in listdir:
-        current_dir = os.path.join(data_dir, subfolder)
-        annotation_dir = os.path.join(current_dir, 'Annotation')
-        img_dir = os.path.join(current_dir, 'Data')
-        list_imgs = [os.path.join(img_dir, dI)
-                     for dI in os.listdir(img_dir)
-                     if (dI.endswith('png')
-                         and not dI.startswith('.'))]
-        list_label_files = [os.path.join(annotation_dir, dI) for dI
-                            in os.listdir(annotation_dir)
-                            if (dI.endswith('txt')
-                                and not dI.startswith('.'))]
-        list_imgs.sort()  # very important to keep the index order
-        list_label_files.sort()
-        print(list_label_files)
-        img_init = np.asarray(Image.open(list_imgs[0]))
-        for label_file in list_label_files:
-            df = pd.read_csv(label_file,
-                             header=None,
-                             names=['id', 'x', 'y'],
-                             sep='\s+')
-            c1_init, c2_init = df.loc[df['id'] == 1, ['x', 'y']].values[0, :]
-            xax, yax = find_template_pixel(c1_init, c2_init,
+    feats_init = None
+    annotation_dir = os.path.join(current_dir, 'Annotation')
+    img_dir = os.path.join(current_dir, 'Data')
+    list_imgs = [os.path.join(img_dir, dI)
+                 for dI in os.listdir(img_dir)
+                 if (dI.endswith('png')
+                     and not dI.startswith('.'))]
+    list_label_files = [os.path.join(annotation_dir, dI) for dI
+                        in os.listdir(annotation_dir)
+                        if (dI.endswith('txt')
+                            and not dI.startswith('.'))]
+    list_imgs.sort()  # very important to keep the index order
+    list_label_files.sort()
+    print(list_label_files)
+    img_init = np.asarray(Image.open(list_imgs[0]))
+    for label_file in list_label_files:
+        df = pd.read_csv(label_file,
+                         header=None,
+                         names=['id', 'x', 'y'],
+                         sep='\s+')
+        c1_init, c2_init = df.loc[df['id'] == 1, ['x', 'y']].values[0, :]
+        xax, yax = find_template_pixel(c1_init, c2_init,
+                                       width=width_template)
+        init_template = img_init[np.ravel(yax), np.ravel(xax)]
+        current_feats_init, _ = np.histogram(
+            init_template, bins=bins, range=(0, 255))
+        n_obs = len(df.id.values)
+        print('id length {}'.format(n_obs))
+        for i in df.id.values[1:n_obs]:
+            img = np.asarray(Image.open(list_imgs[int(i)-1]))
+            c1, c2 = df.loc[df['id'] == i, ['x', 'y']].values[0, :]
+            xax, yax = find_template_pixel(c1, c2,
                                            width=width_template)
-            init_template = img_init[np.ravel(yax), np.ravel(xax)]
-            current_feats_init, _ = np.histogram(
-                init_template, bins=bins, range=(0, 255))
-            n_obs = len(df.id.values)
-            print('id length {}'.format(n_obs))
-            for i in df.id.values[1:n_obs]:
-                img = np.asarray(Image.open(list_imgs[int(i)-1]))
-                c1, c2 = df.loc[df['id'] == i, ['x', 'y']].values[0, :]
-                xax, yax = find_template_pixel(c1, c2,
-                                               width=width_template)
-                img_template = img[np.ravel(yax), np.ravel(xax)]
-                tmp, _ = np.histogram(img_template, bins=bins, range=(0, 255))
-                if feats_intensity is not None:
-                    feats_intensity = np.concatenate(
-                        (feats_intensity, tmp.reshape(1, bins)), axis=0)
-                else:
-                    feats_intensity = np.reshape(tmp, (1, bins))
-            if labels_x is not None:
-                labels_x = np.concatenate(
-                    (labels_x, df.x.values.reshape(-1, 1)), axis=0)
-                labels_y = np.concatenate(
-                    (labels_y, df.y.values.reshape(-1, 1)), axis=0)
-               # x_init = np.concatenate(
-               #     (x_init, np.repeat(df.x.values[0], len(df.id.values))))
-               # y_init = np.concatenate(
-               #     (y_init, np.repeat(df.y.values[0], len(df.id.values))))
-                feats_init = np.concatenate(
-                    (feats_init,
-                        np.tile(current_feats_init, n_obs-1).reshape(-1, bins)),
-                    axis=0)
-                print('feats init shape {}'.format(feats_init.shape))
-                # print('x shape {}'.format(x_init.shape))
-                print('x labels {}'.format(labels_x.shape))
-                print('feats shape {}'.format(feats_intensity.shape))
+            img_template = img[np.ravel(yax), np.ravel(xax)]
+            tmp, _ = np.histogram(img_template, bins=bins, range=(0, 255))
+            if feats_intensity is not None:
+                feats_intensity = np.concatenate(
+                    (feats_intensity, tmp.reshape(1, bins)), axis=0)
             else:
-                labels_x = df.x.values.reshape(-1, 1)
-                labels_y = df.y.values.reshape(-1, 1)
-                #x_init = np.repeat(df.x.values[0], len(df.id.values))
-                #y_init = np.repeat(df.y.values[0], len(df.id.values))
-                feats_init = np.tile(current_feats_init,
-                                     n_obs-1).reshape(-1, bins)
+                feats_intensity = np.reshape(tmp, (1, bins))
+        if labels_x is not None:
+            labels_x = np.concatenate(
+                (labels_x, df.x.values[1:n_obs].reshape(-1, 1)), axis=0)
+            labels_y = np.concatenate(
+                (labels_y, df.y.values[1:n_obs].reshape(-1, 1)), axis=0)
+            feats_init = np.concatenate(
+                (feats_init,
+                    np.tile(current_feats_init, n_obs-1).reshape(-1, bins)),
+                axis=0)
+            print('feats init shape {}'.format(feats_init.shape))
+            print('x labels {}'.format(labels_x.shape))
+            print('feats shape {}'.format(feats_intensity.shape))
+        else:
+            labels_x = df.x.values[1:n_obs].reshape(-1, 1)
+            labels_y = df.y.values[1:n_obs].reshape(-1, 1)
+            feats_init = np.tile(current_feats_init,
+                                 n_obs-1).reshape(-1, bins)
     X_full = np.concatenate((feats_intensity,
                              feats_init), axis=1)
-    #x_init.reshape((-1, 1)),
-    #y_init.reshape((-1, 1)),
+    return X_full, labels_x, labels_y
+
+
+def get_all_local_features(listdir, width_template=60, bins=20):
+    print(listdir)
+    listdir.sort()
+    X_full = None
+    labels_x = None
+    labels_y = None
+    for subfolder in listdir:
+        try:
+            current_X = np.load(os.path.join(subfolder, 'X_{}_{}.npy'.format(width_template, bins)))
+            current_x = np.load(os.path.join(subfolder, 'x_coords.npy'))
+            current_y = np.load(os.path.join(subfolder, 'y_coords.npy'))
+        except FileNotFoundError:
+            current_X, current_x, current_y = get_local_features(
+                subfolder, width_template, bins)
+            np.save(os.path.join(subfolder, 'X_{}_{}.npy'.format(width_template, bins)), current_X)
+            np.save(os.path.join(subfolder, 'x_coords.npy'), current_x)
+            np.save(os.path.join(subfolder, 'y_coords.npy'), current_y)
+        assert(len(current_X)==len(current_x)==len(current_y))
+        if X_full is not None:
+            X_full = np.concatenate((X_full, current_X), axis=0)
+            labels_x = np.concatenate((labels_x, current_x), axis=0)
+            labels_y = np.concatenate((labels_y, current_y), axis=0)
+        else:
+            X_full = current_X
+            labels_x = current_x
+            labels_y = current_y
     return X_full, labels_x, labels_y
 
 
@@ -111,48 +122,52 @@ if __name__ == '__main__':
                                                         test_size=0.33,
                                                         random_state=42)
     """
-    X_full_1, labels_x, labels_y = get_local_features(data_dir,
+    listdir = [os.path.join(data_dir, dI) for dI in os.listdir(
+        data_dir) if (
+        os.path.isdir(os.path.join(data_dir, dI))
+        and not dI == 'feats_matrices')]
+    X_full_1, labels_x, labels_y = get_local_features(listdir,
                                                       width_template=60,
                                                       bins=20)
     np.save(os.path.join(data_dir, 'feats_matrices', 'X_full_60_20'), X_full_1)
     np.save(os.path.join(data_dir, 'feats_matrices', 'labels_x'), labels_x)
     np.save(os.path.join(data_dir, 'feats_matrices', 'labels_y'), labels_y)
-    X_full_2, labels_x_2, labels_y_2 = get_local_features(data_dir,
+    X_full_2, labels_x_2, labels_y_2 = get_local_features(listdir,
                                                           width_template=60,
                                                           bins=40)
     print(np.max(np.abs(labels_x_2-labels_x)))
     np.save(os.path.join(data_dir, 'feats_matrices', 'X_full_60_40'), X_full_2)
-    X_full_3, _, _ = get_local_features(data_dir,
+    X_full_3, _, _ = get_local_features(listdir,
                                         width_template=60,
                                         bins=10)
     np.save(os.path.join(data_dir, 'feats_matrices', 'X_full_60_10'), X_full_3)
-    X_full_4, _, _ = get_local_features(data_dir,
+    X_full_4, _, _ = get_local_features(listdir,
                                         width_template=50,
                                         bins=20)
     np.save(os.path.join(data_dir, 'feats_matrices', 'X_full_50_20'), X_full_4)
-    X_full_5, _, _ = get_local_features(data_dir,
+    X_full_5, _, _ = get_local_features(listdir,
                                         width_template=50,
                                         bins=40)
     np.save(os.path.join(data_dir, 'feats_matrices', 'X_full_50_40'), X_full_5)
-    X_full_6, _, _ = get_local_features(data_dir,
+    X_full_6, _, _ = get_local_features(listdir,
                                         width_template=80,
                                         bins=20)
     np.save(os.path.join(data_dir, 'feats_matrices', 'X_full_80_20'), X_full_6)
-    X_full_7, _, _ = get_local_features(data_dir,
+    X_full_7, _, _ = get_local_features(listdir,
                                         width_template=80,
                                         bins=40)
     np.save(os.path.join(data_dir, 'feats_matrices', 'X_full_80_40'), X_full_7)
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
     meansquare_error = np.zeros((8, 5))
     for i in range(7):
-        est = RandomForestRegressor(n_estimators=1000)
         X = eval('X_full_{}'.format(i+1))
         y = labels_x
         c = 0
         for train_index, test_index in kf.split(X):
+            est = RandomForestRegressor(n_estimators=1000)
             X_train, X_test = X[train_index], X[test_index]
             y_train, y_test = y[train_index], y[test_index]
-            est.fit(X=X_train, y=y_train)
+            est.fit(X=X_train, y=y_train.ravel())
             y_pred = est.predict(X_test)
             meansquare_error[i, c] = np.mean((y_pred - y_test)**2)
             print(np.max(np.abs(y_pred - y_test)))
