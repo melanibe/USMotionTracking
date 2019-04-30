518fb06e9ede9c1a54020a3ed9159c904bef0145
melanibe <melanibe@student.ethz.ch>
2019-04-30 08:09:11 +0200

    new test

--- cross_validation_net.py
+++ cross_validation_net.py
@@ -77,8 +77,8 @@ if __name__=="__main__":
     data_dir = os.getenv('DATA_PATH')
     print(data_dir)
     # default experiment
-    params = {'dropout_rate': 0.4, 'n_epochs': 20,
-                'h3': 0, 'embed_size': 128}
+    params = {'dropout_rate': 0.4, 'n_epochs': 40,
+                'h3': 64, 'embed_size': 128}
     eucl_dist_per_fold, eucl_dist_dict = run_CV(params, data_dir)
     save_res = pd.DataFrame(eucl_dist_dict, index=[0])
     params_df = pd.DataFrame({}, index=[0])
