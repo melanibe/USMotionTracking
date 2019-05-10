60640233e6c9e3b424dcd9c9c1c814e7cfd72f06
melanibe <melanibe@student.ethz.ch>
2019-05-10 10:18:42 +0200

    cv alone new

--- cross_validation_net.py
+++ cross_validation_net.py
@@ -9,7 +9,7 @@ import pandas as pd
 Mélanie Bernhardt - ETH Zurich
 CLUST Challenge
 '''
-
+"""
 def run_CV(params_dict, data_dir, n_splits=5):
     width = params_dict.get('width') if params_dict.get(
         'width') is not None else 60
@@ -74,20 +74,101 @@ def run_CV(params_dict, data_dir, n_splits=5):
     return eucl_dist_per_fold, eucl_dist_dict
     # labels = model.predict(validation_generator)
 
+"""
 
-if __name__=="__main__":
+from dataLoader import DataLoader, compute_euclidean_distance, prepare_input_img
+from network import create_model
+import os
+import numpy as np
+from custom_KFold import MyKFold
+from block_matching_utils import find_template_pixel, NCC_best_template_search
+from PIL import Image
+import pandas as pd
+from tensorflow import keras
+from utils import get_logger, get_default_params
+import skimage
+import tensorflow as tf
+
+'''
+Mélanie Bernhardt - ETH Zurich
+CLUST Challenge
+'''
+
+def run_cv(fold_iterator, logger, params_dict):
+    for traindirs, testdirs in fold_iterator:
+        # TRAIN LOCAL PREDICTION MODEL
+        # Generators
+        logger.info('############ FOLD #############')
+        logger.info('Training folders are {}'.format(traindirs))
+        training_generator = DataLoader(
+            data_dir, traindirs, 32,
+            width_template=params_dict['width'])
+        validation_generator = DataLoader(
+            data_dir, testdirs, 32,
+            width_template=params_dict['width'],
+            type='val')
+
+        # Design model
+        model = create_model(params_dict['width']+1,
+                             params_dict['h1'],
+                             params_dict['h2'],
+                             params_dict['h3'],
+                             embed_size=params_dict['embed_size'],
+                             drop_out_rate=params_dict['dropout_rate'],
+                             use_batch_norm=params_dict['use_batchnorm'])
+        # Train model on training dataset
+        '''
+        model.fit_generator(generator=training_generator,
+                            validation_data=validation_generator,
+                            use_multiprocessing=True,
+                            epochs=params_dict['n_epochs'],
+                            workers=6)
+        '''
+        try:
+            model.load_weights(os.path.join(checkpoint_dir, 'model22.h5'))
+        except OSError:
+            print('here')
+            model.fit_generator(generator=training_generator,
+                                validation_data=validation_generator,
+                                use_multiprocessing=True,
+                                epochs=params_dict['n_epochs'],
+                                workers=4, max_queue_size=20)
+            model.save_weights(os.path.join(checkpoint_dir, 'model.h5'))
+        metrics = model.evaluate_generator(generator=validation_generator, workers=4, max_queue_size=20)
+        logger.info(metrics)
+
+if __name__ == '__main__':
     np.random.seed(seed=42)
-    # Get the training data
+    exp_name = 'cv_alone_1'
+    params_dict = {'dropout_rate': 0.5, 'n_epochs': 10,
+                   'h3': 0, 'embed_size': 128, 'width': 100, 'search_w': 50}
+
+    # ============ DATA AND SAVING DIRS SETUP ========== #
     data_dir = os.getenv('DATA_PATH')
-    print(data_dir)
-    # default experiment
-    params = {'dropout_rate': 0.5, 'n_epochs': 80,
-            'h3': 0, 'embed_size': 512, 'width': 60}
-    eucl_dist_per_fold, eucl_dist_dict = run_CV(params, data_dir)
-    save_res = pd.DataFrame(eucl_dist_dict, index=[0])
-    params_df = pd.DataFrame({}, index=[0])
-    save_res['mean_over_folds'] = np.mean(eucl_dist_per_fold)
-    save_res['std_over_folds'] = np.std(eucl_dist_per_fold)
-    result = pd.concat([save_res, params_df], axis=1)
-    result.to_csv(os.path.join('data_dir', 'results', 'default.csv'), index=False)
-    print(result)
+    exp_dir = os.getenv('EXP_PATH')
+    checkpoint_dir = os.path.join(exp_dir, exp_name)
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    # ============= LOGGER SETUP ================= #
+    # create logger
+    logger = get_logger(checkpoint_dir)
+
+    # Set the default parameters
+    params_dict = get_default_params(params_dict)
+
+    # ========= PRINT CONFIG TO LOG ======== #
+    logger.info('Running %s experiment ...' % exp_name)
+    logger.info('\n Settings for this expriment are: \n')
+    for key in params_dict.keys():
+        logger.info('  {}: {}'.format(key.upper(), params_dict[key]))
+    logger.info('Saving checkpoint to {}'.format(checkpoint_dir))
+
+    # KFold iterator
+    kf = MyKFold(data_dir, n_splits=5)
+    fold_iterator = kf.getFolderIterator()
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.666)
+
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+    tf.keras.backend.set_session(sess)
+    run_cv(fold_iterator, logger, params_dict)
+
